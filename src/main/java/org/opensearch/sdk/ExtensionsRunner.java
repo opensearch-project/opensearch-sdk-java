7af4f075757b292ae21f590df59419328b3f2dff
Owais Kazi <owaiskazi19@gmail.com>
2022-08-23 10:13:59 -0700

    Created SDK Client to interact with OpenSearch (#67)
    * Integrated SDK Client to make request to OpenSearch
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Added test and restructure SDK
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Added license
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Send config from initial request
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Utilized the hostaddress and port from extension initialize request
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Integrated SDK Client to make request to OpenSearch
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Added test and restructure SDK
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Removed security related attributes
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Updated branch
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * PR comments
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Changed the method name
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Small changes
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>

--- src/main/java/org/opensearch/sdk/ExtensionsRunner.java
+++ src/main/java/org/opensearch/sdk/ExtensionsRunner.java
@@ -71,6 +71,8 @@ public class ExtensionsRunner {
     private String uniqueId;
     private DiscoveryNode opensearchNode;
     private TransportService extensionTransportService = null;
+    private int port;
+    private String hostAddress;
 
     private final Settings settings = Settings.builder()
         .put("node.name", extensionSettings.getExtensionName())
@@ -143,6 +145,8 @@ public class ExtensionsRunner {
             return initializeExtensionsResponse;
         } finally {
             // After sending successful response to initialization, send the REST API
+            port = opensearchNode.getAddress().getPort();
+            hostAddress = opensearchNode.getAddress().getAddress();
             setOpensearchNode(opensearchNode);
             extensionTransportService.connectToNode(opensearchNode);
             sendRegisterRestActionsRequest(extensionTransportService);
---
+++ src/main/java/org/opensearch/sdk/SDKClient.java
@@ -0,0 +1,67 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.sdk;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestClientBuilder;
+import org.opensearch.client.json.jackson.JacksonJsonpMapper;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.OpenSearchTransport;
+import org.opensearch.client.transport.rest_client.RestClientTransport;
+
+/**
+ * This class creates SDKClient for an extension to make requests to OpenSearch
+ */
+public class SDKClient {
+    private final Logger logger = LogManager.getLogger(SDKClient.class);
+    private OpenSearchClient javaClient;
+    private RestClient restClient = null;
+
+    /**
+     * Creates OpenSearchClient for SDK. It also creates a restClient as a wrapper around Java OpenSearchClient
+     * @param hostAddress The address of OpenSearch cluster, client can connect to
+     * @param port The port of OpenSearch cluster
+     * @throws IOException if client failed
+     * @return SDKClient which is internally an OpenSearchClient. The user is responsible for calling {@link #doCloseRestClient()} when finished with the client
+     */
+    public OpenSearchClient initializeClient(String hostAddress, int port) throws IOException {
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostAddress, port));
+        builder.setStrictDeprecationMode(true);
+        builder.setHttpClientConfigCallback(httpClientBuilder -> {
+            try {
+                return httpClientBuilder.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        restClient = builder.build();
+
+        // Create Client
+        OpenSearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        javaClient = new OpenSearchClient(transport);
+        return javaClient;
+    }
+
+    /**
+     *
+     * @throws IOException if closing the restClient fails
+     */
+    public void doCloseRestClient() throws IOException {
+        if (restClient != null) {
+            restClient.close();
+        }
+    }
+}
