160cf0aa998e2696d45dce6b8d464e3f9d19f342
Craig Perkins <cwperx@amazon.com>
2022-09-02 11:26:53 -0400

    Update CrudExtension with recent RestResponse change
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- src/main/java/org/opensearch/sdk/sample/crud/rest/RestCreateAction.java
+++ src/main/java/org/opensearch/sdk/sample/crud/rest/RestCreateAction.java
@@ -7,8 +7,10 @@
  */
 package org.opensearch.sdk.sample.crud.rest;
 
+import org.opensearch.rest.BytesRestResponse;
 import org.opensearch.rest.RestHandler.Route;
 import org.opensearch.rest.RestRequest.Method;
+import org.opensearch.rest.RestResponse;
 import org.opensearch.sdk.ExtensionRestHandler;
 import org.opensearch.sdk.authz.RequiresPermissions;
 
@@ -16,6 +18,8 @@ import java.util.List;
 
 import static java.util.Collections.singletonList;
 import static org.opensearch.rest.RestRequest.Method.PUT;
+import static org.opensearch.rest.RestStatus.INTERNAL_SERVER_ERROR;
+import static org.opensearch.rest.RestStatus.OK;
 
 /**
  * Sample REST Handler (REST Action). Extension REST handlers must implement {@link ExtensionRestHandler}.
@@ -35,12 +39,15 @@ public class RestCreateAction implements ExtensionRestHandler {
         permissions = { "extensions:sample/crud/create" }
     )
     @Override
-    public String handleRequest(Method method, String uri) {
+    public RestResponse handleRequest(Method method, String uri) {
         // TODO modify RestExecuteOnExtensionRequest to get request body. (Should it get params and headers too?)
         if (Method.PUT.equals(method) && "/crud/create".equals(uri)) {
-            return SUCCESSFUL;
+            return new BytesRestResponse(OK, SUCCESSFUL);
         }
-        return null;
+        return new BytesRestResponse(
+            INTERNAL_SERVER_ERROR,
+            "Extension REST action improperly configured to handle " + method.name() + " " + uri
+        );
     }
 
 }
--- src/main/java/org/opensearch/sdk/sample/crud/rest/RestUpdateAction.java
+++ src/main/java/org/opensearch/sdk/sample/crud/rest/RestUpdateAction.java
@@ -7,8 +7,10 @@
  */
 package org.opensearch.sdk.sample.crud.rest;
 
+import org.opensearch.rest.BytesRestResponse;
 import org.opensearch.rest.RestHandler.Route;
 import org.opensearch.rest.RestRequest.Method;
+import org.opensearch.rest.RestResponse;
 import org.opensearch.sdk.ExtensionRestHandler;
 import org.opensearch.sdk.authz.RequiresPermissions;
 
@@ -16,6 +18,8 @@ import java.util.List;
 
 import static java.util.Collections.singletonList;
 import static org.opensearch.rest.RestRequest.Method.POST;
+import static org.opensearch.rest.RestStatus.INTERNAL_SERVER_ERROR;
+import static org.opensearch.rest.RestStatus.OK;
 
 /**
  * Sample REST Handler (REST Action). Extension REST handlers must implement {@link ExtensionRestHandler}.
@@ -38,11 +42,14 @@ public class RestUpdateAction implements ExtensionRestHandler {
 
     // TODO modify RestExecuteOnExtensionRequest to get request body. (Should it get params and headers too?)
     @Override
-    public String handleRequest(Method method, String uri) {
+    public RestResponse handleRequest(Method method, String uri) {
         if (Method.POST.equals(method) && "/crud/update".equals(uri)) {
-            return SUCCESSFUL;
+            return new BytesRestResponse(OK, SUCCESSFUL);
         }
-        return null;
+        return new BytesRestResponse(
+                INTERNAL_SERVER_ERROR,
+                "Extension REST action improperly configured to handle " + method.name() + " " + uri
+        );
     }
 
 }
