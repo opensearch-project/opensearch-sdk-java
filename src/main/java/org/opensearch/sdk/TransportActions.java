0d49b29b1e4c5d2a17a199fb353846f819b627b3
Daniel Widdis <widdis@gmail.com>
2022-09-18 17:51:09 -0700

    Fix javadoc warnings
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/main/java/org/opensearch/sdk/TransportActions.java
+++ src/main/java/org/opensearch/sdk/TransportActions.java
@@ -30,6 +30,9 @@ public class TransportActions {
 
     /**
      * Constructor for TransportActions. Creates a map of transportActions for this extension.
+     *
+     * @param <Request> the TransportAction request
+     * @param <Response> the TransportAction response
      * @param transportActions is the list of actions the extension would like to register with OpenSearch.
      */
     public <Request extends ActionRequest, Response extends ActionResponse> TransportActions(
