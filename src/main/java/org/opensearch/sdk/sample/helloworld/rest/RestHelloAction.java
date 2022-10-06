65381c623a9bbfb5e2afd1af3ca1010ec6598f0d
Daniel Widdis <widdis@gmail.com>
2022-10-06 13:41:27 -0700

    Add a POST request to parse content
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/main/java/org/opensearch/sdk/sample/helloworld/rest/RestHelloAction.java
+++ src/main/java/org/opensearch/sdk/sample/helloworld/rest/RestHelloAction.java
@@ -7,6 +7,7 @@
  */
 package org.opensearch.sdk.sample.helloworld.rest;
 
+import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.extensions.rest.ExtensionRestRequest;
 import org.opensearch.extensions.rest.ExtensionRestResponse;
 import org.opensearch.rest.RestHandler.Route;
@@ -14,11 +15,15 @@ import org.opensearch.rest.RestRequest.Method;
 import org.opensearch.sdk.ExtensionRestHandler;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static org.opensearch.rest.RestRequest.Method.GET;
+import static org.opensearch.rest.RestRequest.Method.POST;
 import static org.opensearch.rest.RestRequest.Method.PUT;
 import static org.opensearch.rest.RestStatus.BAD_REQUEST;
+import static org.opensearch.rest.RestStatus.NOT_ACCEPTABLE;
 import static org.opensearch.rest.RestStatus.NOT_FOUND;
 import static org.opensearch.rest.RestStatus.OK;
 
@@ -29,10 +34,12 @@ public class RestHelloAction implements ExtensionRestHandler {
 
     private static final String GREETING = "Hello, %s!";
     private String worldName = "World";
+    private List<String> worldAdjectives = new ArrayList<>();
+    private Random rand = new Random();
 
     @Override
     public List<Route> routes() {
-        return List.of(new Route(GET, "/hello"), new Route(PUT, "/hello/{name}"));
+        return List.of(new Route(GET, "/hello"), new Route(POST, "/hello"), new Route(PUT, "/hello/{name}"));
     }
 
     @Override
@@ -41,6 +48,8 @@ public class RestHelloAction implements ExtensionRestHandler {
 
         if (Method.GET.equals(method)) {
             return handleGetRequest(request);
+        } else if (Method.POST.equals(method)) {
+            return handlePostRequest(request);
         } else if (Method.PUT.equals(method)) {
             return handlePutRequest(request);
         }
@@ -48,7 +57,42 @@ public class RestHelloAction implements ExtensionRestHandler {
     }
 
     private ExtensionRestResponse handleGetRequest(ExtensionRestRequest request) {
-        return new ExtensionRestResponse(request, OK, String.format(GREETING, worldName));
+        String worldNameWithRandomAdjective = worldAdjectives.isEmpty()
+            ? worldName
+            : String.join(" ", worldAdjectives.get(rand.nextInt(worldAdjectives.size())), worldName);
+        return new ExtensionRestResponse(request, OK, String.format(GREETING, worldNameWithRandomAdjective));
+    }
+
+    private ExtensionRestResponse handlePostRequest(ExtensionRestRequest request) {
+        if (request.hasContent()) {
+            XContentType contentType = request.getXContentType();
+            if (contentType == null) {
+                // Plain text
+                String adjective = request.content().utf8ToString();
+                worldAdjectives.add(adjective);
+                return new ExtensionRestResponse(request, OK, "Added " + adjective + " to words that describe the world!");
+            } else if (contentType.equals(XContentType.JSON)) {
+                // TODO: Once CreateComponents has an XContentRegistry available we can parse from there
+                // For now we just hack our way into the result.
+                String json = request.content().utf8ToString();
+                boolean foundLabel = false;
+                boolean foundColon = false;
+                for (String s : json.split("\"")) {
+                    if (!foundLabel) {
+                        foundLabel = "adjective".equals(s);
+                    } else if (!foundColon) {
+                        foundColon = s.contains(":");
+                    } else {
+                        // This is the adjective!
+                        worldAdjectives.add(s);
+                        return new ExtensionRestResponse(request, OK, "Added " + s + " to words that describe the world!");
+                    }
+                }
+                return new ExtensionRestResponse(request, OK, request.content().utf8ToString());
+            }
+            return new ExtensionRestResponse(request, NOT_ACCEPTABLE, "Only text and JSON content types are supported");
+        }
+        return new ExtensionRestResponse(request, BAD_REQUEST, "No content included with POST request");
     }
 
     private ExtensionRestResponse handlePutRequest(ExtensionRestRequest request) {
