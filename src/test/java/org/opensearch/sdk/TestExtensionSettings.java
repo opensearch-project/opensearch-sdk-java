d4586473326cd9e4b9c8f8ed135e28464eba8605
Ryan Bogan <rbogan@amazon.com>
2023-02-14 23:14:01 +0000

    Replace jackson YAML reading with snakeyaml
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- src/test/java/org/opensearch/sdk/TestExtensionSettings.java
+++ src/test/java/org/opensearch/sdk/TestExtensionSettings.java
@@ -9,27 +9,21 @@
 
 package org.opensearch.sdk;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.opensearch.test.OpenSearchTestCase;
 
-import java.io.File;
 import java.io.IOException;
 
 public class TestExtensionSettings extends OpenSearchTestCase {
     private static final String EXTENSION_DESCRIPTOR_CLASSPATH = "/extension.yml";
-    private static final String EXTENSION_DESCRIPTOR_FILEPATH = "src/test/resources" + EXTENSION_DESCRIPTOR_CLASSPATH;
     private ExtensionSettings extensionSettings;
 
     @Override
     @BeforeEach
     public void setUp() throws Exception {
         super.setUp();
-        File file = new File(EXTENSION_DESCRIPTOR_FILEPATH);
-        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-        extensionSettings = objectMapper.readValue(file, ExtensionSettings.class);
+        extensionSettings = ExtensionSettings.readSettingsFromYaml(EXTENSION_DESCRIPTOR_CLASSPATH);
     }
 
     @Test
@@ -60,7 +54,7 @@ public class TestExtensionSettings extends OpenSearchTestCase {
         assertEquals(extensionSettings.getHostPort(), settings.getHostPort());
         assertEquals(extensionSettings.getOpensearchAddress(), settings.getOpensearchAddress());
         assertEquals(extensionSettings.getOpensearchPort(), settings.getOpensearchPort());
-        assertNull(ExtensionSettings.readSettingsFromYaml("this/path/does/not/exist"));
-        assertNull(ExtensionSettings.readSettingsFromYaml(EXTENSION_DESCRIPTOR_CLASSPATH + "filedoesnotexist"));
+        expectThrows(IOException.class, () -> ExtensionSettings.readSettingsFromYaml("this/path/does/not/exist"));
+        expectThrows(IOException.class, () -> ExtensionSettings.readSettingsFromYaml(EXTENSION_DESCRIPTOR_CLASSPATH + "filedoesnotexist"));
     }
 }
