831861985d3b1ceaeca90c3874dbe0fae27a83d8
Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
2023-03-08 00:39:16 +0600

    Created ScriptExtension interface #318
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>

---
+++ src/main/java/org/opensearch/sdk/ScriptExtension.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.sdk;
+
+import org.opensearch.common.settings.Settings;
+import org.opensearch.script.ScriptContext;
+import org.opensearch.script.ScriptEngine;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * An additional extension point for {@link Extension}s that extends OpenSearch's scripting functionality.
+ *
+ * @opensearch.api
+ */
+public interface ScriptExtension {
+
+    /**
+     * Returns a {@link ScriptEngine} instance or <code>null</code> if this extension doesn't add a new script engine.
+     * @param settings Node settings
+     * @param contexts The contexts that {@link ScriptEngine#compile(String, String, ScriptContext, Map)} may be called with
+     */
+    default ScriptEngine getScriptEngine(Settings settings, Collection<ScriptContext<?>> contexts) {
+        return null;
+    }
+
+    /**
+     * Return script contexts this extension wants to allow using.
+     */
+    default List<ScriptContext<?>> getContexts() {
+        return Collections.emptyList();
+    }
+}
