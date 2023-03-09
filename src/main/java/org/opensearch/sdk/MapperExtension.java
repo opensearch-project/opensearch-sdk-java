39f113205e955d3942440db44def47940ed0c42f
Kuanysh <kuanysh4646@gmail.com>
2023-03-09 22:30:43 +0600

    deleted comments
    Signed-off-by: Kuanysh <kuanysh4646@gmail.com>

--- src/main/java/org/opensearch/sdk/MapperExtension.java
+++ src/main/java/org/opensearch/sdk/MapperExtension.java
@@ -10,47 +10,21 @@ import java.util.function.Predicate;
 
 public interface MapperExtension {
 
-    /**
-     * Returns additional mapper implementations added by this extension.
-     * <p>
-     * The key of the returned {@link Map} is the unique name for the mapper which will be used
-     * as the mapping {@code type}, and the value is a {@link Mapper.TypeParser} to parse the
-     * mapper settings into a {@link Mapper}.
-     */
     default Map<String, Mapper.TypeParser> getMappers() {
         return Collections.emptyMap();
     }
 
-    /**
-     * Returns additional metadata mapper implementations added by this extension.
-     * <p>
-     * The key of the returned {@link Map} is the unique name for the metadata mapper, which
-     * is used in the mapping json to configure the metadata mapper, and the value is a
-     * {@link MetadataFieldMapper.TypeParser} to parse the mapper settings into a
-     * {@link MetadataFieldMapper}.
-     */
+
     default Map<String, MetadataFieldMapper.TypeParser> getMetadataMappers() {
         return Collections.emptyMap();
     }
 
-    /**
-     * Returns a function that given an index name returns a predicate which fields must match in order to be returned by get mappings,
-     * get index, get field mappings and field capabilities API. Useful to filter the fields that such API return. The predicate receives
-     * the field name as input argument and should return true to show the field and false to hide it.
-     */
+
     default Function<String, Predicate<String>> getFieldFilter() {
         return NOOP_FIELD_FILTER;
     }
 
-    /**
-     * The default field predicate applied, which doesn't filter anything. That means that by default get mappings, get index
-     * get field mappings and field capabilities API will return every field that's present in the mappings.
-     */
     Predicate<String> NOOP_FIELD_PREDICATE = field -> true;
 
-    /**
-     * The default field filter applied, which doesn't filter anything. That means that by default get mappings, get index
-     * get field mappings and field capabilities API will return every field that's present in the mappings.
-     */
     Function<String, Predicate<String>> NOOP_FIELD_FILTER = index -> NOOP_FIELD_PREDICATE;
 }
