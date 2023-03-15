7cece55d3365e667f55a61c5ed758529ac1363c6
Daulet Nassipkali <uralskdev@gmail.com>
2023-03-15 21:31:29 +0500

    Added SystemIndexExtension (#572)
    * Added SystemIndexExtension interface
    
    Signed-off-by: Daulet <uralskdev@gmail.com>
    
    * changes after spotlessApply
    
    Signed-off-by: Daulet <uralskdev@gmail.com>
    
    ---------
    
    Signed-off-by: Daulet <uralskdev@gmail.com>
    Signed-off-by: Nurym <nurym0807@gmail.com>

---
+++ src/main/java/org/opensearch/sdk/SystemIndexExtension.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.sdk;
+
+import org.opensearch.common.settings.Settings;
+import org.opensearch.indices.SystemIndexDescriptor;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Extension for defining system indices. Extends {@link ActionExtension} because system indices must be accessed via APIs
+ * added by the extension that owns the system index, rather than standard APIs.
+ *
+ *
+ */
+public interface SystemIndexExtension extends ActionExtension {
+
+    /**
+     * Returns a {@link Collection} of {@link SystemIndexDescriptor}s that describe this plugin's system indices, including
+     * name, mapping, and settings.
+     * @param settings The node's settings
+     * @return Descriptions of the system indices managed by this plugin.
+     */
+    default Collection<SystemIndexDescriptor> getSystemIndexDescriptors(Settings settings) {
+        return Collections.emptyList();
+    }
+}
