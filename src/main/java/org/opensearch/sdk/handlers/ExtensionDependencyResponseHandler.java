124d26bf9f54d375306a9e1f84bb8e33cd71425a
Kuanysh <kuanysh4646@gmail.com>
2023-03-18 23:56:30 +0600

    fix bugs
    Signed-off-by: Kuanysh <kuanysh4646@gmail.com>

--- src/main/java/org/opensearch/sdk/handlers/ExtensionDependencyResponseHandler.java
+++ src/main/java/org/opensearch/sdk/handlers/ExtensionDependencyResponseHandler.java
@@ -38,7 +38,6 @@ public class ExtensionDependencyResponseHandler implements TransportResponseHand
      */
     public ExtensionDependencyResponseHandler() {
         this.inProgressFuture = new CompletableFuture<>();
-        this.extensions = extensions;
     }
 
     @Override
--- src/main/java/org/opensearch/sdk/handlers/UpdateSettingsRequestHandler.java
+++ src/main/java/org/opensearch/sdk/handlers/UpdateSettingsRequestHandler.java
@@ -99,9 +99,9 @@ public class UpdateSettingsRequestHandler {
                 case Version:
                     Consumer<Version> versionConsumer = (Consumer<Version>) this.settingUpdateConsumers.get(componentSetting);
                     versionConsumer.accept((Version) data);
-                default:
-                    throw new UnsupportedOperationException("Setting Update Consumer type does not exist and is not handled here");
             }
+        } catch (UnsupportedOperationException e) {
+            throw new UnsupportedOperationException("Setting Update Consumer type does not exist and is not handled here");
         } catch (Exception e) {
             logger.info(e.getMessage());
             settingUpdateStatus = false;
