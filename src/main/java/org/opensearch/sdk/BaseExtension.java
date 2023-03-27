e13aed8eaaf17f448a9783b8fcb6a3f20ef6d5bb
petiveriaalliacea <77691894+petiveriaalliacea@users.noreply.github.com>
2023-03-28 02:22:17 +0600

    Added getNamedWriteableRegistry() API for extensions (#553)
    * Created ScriptExtension interface #318
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Created ScriptExtension interface #318
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Created ScriptExtension interface #318
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Created ScriptExtension interface #318
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Created ScriptExtension interface #318
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Created ScriptExtension interface #318
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Fixed Tests. Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Fixed Tests. Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Fixed Tests. Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Fixed Tests. Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    * Fixed Tests. Added getNamedWriteableRegistry() API for extensions #291
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    
    ---------
    
    Signed-off-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    Signed-off-by: petiveriaalliacea <77691894+petiveriaalliacea@users.noreply.github.com>
    Co-authored-by: Aisara Imangaliyeva <imangaliyeva.aisara@gmail.com>
    Co-authored-by: Ryan Bogan <10944539+ryanbogan@users.noreply.github.com>

--- src/main/java/org/opensearch/sdk/BaseExtension.java
+++ src/main/java/org/opensearch/sdk/BaseExtension.java
@@ -15,6 +15,7 @@ import java.io.IOException;
  * An abstract class that simplifies extension initialization and provides an instance of the runner.
  */
 public abstract class BaseExtension implements Extension {
+
     /**
      * The {@link ExtensionsRunner} instance running this extension
      */
