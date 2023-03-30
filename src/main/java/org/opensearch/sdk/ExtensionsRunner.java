f3cf478ca6ca8b240f03f942991a8ef50ee0831c
Daniel Widdis <widdis@gmail.com>
2023-03-30 11:02:24 -0700

    Generate RestRequest to pass to Rest Handlers (#623)
    * Generate RestRequest to pass to Rest Handlers
    
    * Update Response handling to use RestRequest (needs companion PR)
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Update Extension with new SDK signature (preview of AD extension PR)
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Less diff on migration, yay!
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Remove stray reference to old type
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * Typo fix
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    * CharSequence isEmpty() is a JDK 15+ feature
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/main/java/org/opensearch/sdk/ExtensionsRunner.java
+++ src/main/java/org/opensearch/sdk/ExtensionsRunner.java
@@ -152,7 +152,7 @@ public class ExtensionsRunner {
     private final ExtensionsIndicesModuleRequestHandler extensionsIndicesModuleRequestHandler = new ExtensionsIndicesModuleRequestHandler();
     private final ExtensionsIndicesModuleNameRequestHandler extensionsIndicesModuleNameRequestHandler =
         new ExtensionsIndicesModuleNameRequestHandler();
-    private final ExtensionsRestRequestHandler extensionsRestRequestHandler = new ExtensionsRestRequestHandler(extensionRestPathRegistry);
+    private final ExtensionsRestRequestHandler extensionsRestRequestHandler;
     private final AtomicReference<RunnableTaskExecutionListener> runnableTaskListener;
     private final IndexNameExpressionResolver indexNameExpressionResolver;
     private final ExtensionActionRequestHandler extensionsActionRequestHandler;
@@ -195,8 +195,10 @@ public class ExtensionsRunner {
         this.customNamedXContent = extension.getNamedXContent();
         // save custom namedWriteable
         this.customNamedWriteables = extension.getNamedWriteables();
-        // initialize NamedXContent Registry. Must happen after getting extension namedXContent
+        // initialize NamedXContent Registry.
         this.sdkNamedXContentRegistry = new SDKNamedXContentRegistry(this);
+        // initialize RestRequest Handler. Must happen after instantiating SDKNamedXContentRegistry
+        this.extensionsRestRequestHandler = new ExtensionsRestRequestHandler(extensionRestPathRegistry, sdkNamedXContentRegistry);
         // initialize NamedWriteable Registry. Must happen after getting extension namedWriteable
         this.sdkNamedWriteableRegistry = new SDKNamedWriteableRegistry(this);
 
