a921cf182826b608e3d403ab4d2c6be3c13beb32
Ryan Bogan <rbogan@amazon.com>
2023-05-17 17:44:55 +0000

    Debug test commit
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

---
+++ src/main/java/org/opensearch/sdk/SDKPlugin.java
@@ -0,0 +1,9 @@
+package org.opensearch.sdk;
+
+import org.opensearch.plugins.Plugin;
+
+public class SDKPlugin extends Plugin {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
