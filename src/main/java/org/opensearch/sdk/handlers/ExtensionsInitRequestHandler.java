34096132c0b4d7cd7fabbcd7f82417029121bb6c
Daniel Widdis <widdis@gmail.com>
2023-05-18 14:22:18 -0700

    Add default Opensearch host and port if init comes from localhost
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/main/java/org/opensearch/sdk/handlers/ExtensionsInitRequestHandler.java
+++ src/main/java/org/opensearch/sdk/handlers/ExtensionsInitRequestHandler.java
@@ -12,14 +12,22 @@ package org.opensearch.sdk.handlers;
 import org.apache.logging.log4j.LogManager;
 
 import org.apache.logging.log4j.Logger;
+import org.opensearch.cluster.node.DiscoveryNode;
 import org.opensearch.common.settings.Settings;
+import org.opensearch.common.transport.TransportAddress;
 import org.opensearch.discovery.InitializeExtensionRequest;
 import org.opensearch.discovery.InitializeExtensionResponse;
+import org.opensearch.extensions.DiscoveryExtensionNode;
 import org.opensearch.sdk.ExtensionsRunner;
 import org.opensearch.sdk.SDKTransportService;
 import org.opensearch.transport.TransportService;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import static org.opensearch.sdk.ExtensionsRunner.NODE_NAME_SETTING;
+import static org.opensearch.sdk.ExtensionsRunner.OPENSEARCH_HOST_SETTING;
+import static org.opensearch.sdk.ExtensionsRunner.OPENSEARCH_PORT_SETTING;
 
 /**
  * This class handles the request from OpenSearch to a {@link ExtensionsRunner#startTransportService(TransportService transportService)} call.
@@ -46,14 +54,33 @@ public class ExtensionsInitRequestHandler {
      * @return A response to OpenSearch validating that this is an extension.
      */
     public InitializeExtensionResponse handleExtensionInitRequest(InitializeExtensionRequest extensionInitRequest) {
-        logger.info("Registering Extension Request received from OpenSearch");
-        extensionsRunner.opensearchNode = extensionInitRequest.getSourceNode();
-        extensionsRunner.getThreadPool().getThreadContext().putHeader("extension_unique_id", extensionInitRequest.getExtension().getId());
-        extensionsRunner.setUniqueId(extensionInitRequest.getExtension().getId());
-        // TODO: Remove above two lines in favor of the below when refactoring
+        DiscoveryNode sourceNode = extensionInitRequest.getSourceNode();
+        DiscoveryExtensionNode extensionNode = extensionInitRequest.getExtension();
+        logger.info("Registering Extension Request received from OpenSearch node {}", sourceNode);
+        // In some cases the source node has a localhost IP address which is unreachable from extension.
+        String sourceHost = sourceNode.getAddress().address().getHostString();
+        if ("127.0.0.1".equals(sourceHost)) {
+            // pull from ExtensionSettings instead
+            sourceHost = extensionsRunner.getSettings().get(OPENSEARCH_HOST_SETTING);
+            try {
+                int sourcePort = Integer.valueOf(extensionsRunner.getSettings().get(OPENSEARCH_PORT_SETTING));
+                sourceNode = new DiscoveryNode(
+                    sourceNode.getName(),
+                    new TransportAddress(InetAddress.getByName(sourceHost), sourcePort),
+                    sourceNode.getVersion()
+                );
+            } catch (NumberFormatException | UnknownHostException e) {
+                // If configured defaults don't work, just keep original source node
+            }
+        }
+        extensionsRunner.opensearchNode = sourceNode;
+        extensionsRunner.getThreadPool().getThreadContext().putHeader("extension_unique_id", extensionNode.getId());
+        extensionsRunner.setUniqueId(extensionNode.getId());
+        // TODO: Remove above lines setting extensionRunner node and ID in favor of the below when refactoring
+        // per https://github.com/opensearch-project/opensearch-sdk-java/issues/585
         SDKTransportService sdkTransportService = extensionsRunner.getSdkTransportService();
-        sdkTransportService.setOpensearchNode(extensionInitRequest.getSourceNode());
-        sdkTransportService.setUniqueId(extensionInitRequest.getExtension().getId());
+        sdkTransportService.setOpensearchNode(sourceNode);
+        sdkTransportService.setUniqueId(extensionNode.getId());
         // Successfully initialized. Send the response.
         try {
             return new InitializeExtensionResponse(
@@ -62,7 +89,7 @@ public class ExtensionsInitRequestHandler {
             );
         } finally {
             // After sending successful response to initialization, send the REST API and Settings
-            extensionsRunner.setOpensearchNode(extensionInitRequest.getSourceNode());
+            extensionsRunner.setOpensearchNode(sourceNode);
             extensionsRunner.setExtensionNode(extensionInitRequest.getExtension());
             extensionsRunner.getSdkClient().updateOpenSearchNodeSettings(extensionInitRequest.getSourceNode().getAddress());
 
