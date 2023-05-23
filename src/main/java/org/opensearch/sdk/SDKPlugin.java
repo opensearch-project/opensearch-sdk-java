1aa03ec021ebf60842b831cf7ba25bc317a99bdd
Ryan Bogan <rbogan@amazon.com>
2023-05-23 20:56:59 +0000

    Remove extra test class
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- src/main/java/org/opensearch/sdk/SDKPlugin.java
+++
@@ -1,9 +0,0 @@
-package org.opensearch.sdk;
-
-import org.opensearch.plugins.Plugin;
-
-public class SDKPlugin extends Plugin {
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
-}
