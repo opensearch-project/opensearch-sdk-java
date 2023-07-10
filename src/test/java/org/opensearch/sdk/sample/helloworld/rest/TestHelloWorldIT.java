3359a607a25c3f83a7e44fc660f19562990a1114
Sarat Vemulapalli <vemulapallisarat@gmail.com>
2023-07-10 16:10:15 -0700

    Adding spotless changes
    Signed-off-by: Sarat Vemulapalli <vemulapallisarat@gmail.com>

--- src/test/java/org/opensearch/sdk/sample/helloworld/rest/TestHelloWorldIT.java
+++ src/test/java/org/opensearch/sdk/sample/helloworld/rest/TestHelloWorldIT.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
 package org.opensearch.sdk.sample.helloworld.rest;
 
 import org.junit.jupiter.api.Test;
@@ -12,13 +21,13 @@ public class TestHelloWorldIT extends OpenSearchRestTestCase {
     @Test
     public void testInitHelloWorld() throws IOException {
         Request request = new Request("PUT", "/_extensions/initialize");
-        String entity = "{\"name\": \"hello-world\",\n" +
-                "  \"uniqueId\": \"hello-world\",\n" +
-                "  \"hostAddress\": \"127.0.0.1\",\n" +
-                "  \"port\": \"4500\",\n" +
-                "  \"version\": \"1.0\",\n" +
-                "  \"opensearchVersion\": \"2.9.0\",\n" +
-                "  \"minimumCompatibleVersion\": \"2.9.0\",}";
+        String entity = "{\"name\": \"hello-world\",\n"
+            + "  \"uniqueId\": \"hello-world\",\n"
+            + "  \"hostAddress\": \"127.0.0.1\",\n"
+            + "  \"port\": \"4500\",\n"
+            + "  \"version\": \"1.0\",\n"
+            + "  \"opensearchVersion\": \"2.9.0\",\n"
+            + "  \"minimumCompatibleVersion\": \"2.9.0\",}";
         request.setJsonEntity(entity);
         Response response = client().performRequest(request);
         assertEquals("HTTP/1.1 200 OK", response.getStatusLine());
